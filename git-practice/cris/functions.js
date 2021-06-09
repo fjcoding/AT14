88ebd5a2753d52de86fdbc29557489585fd20fc9
MrRoaming <crist.v095@gmail.com>
2021-06-09 07:56:53 -0400

    adding some string operations

--- git-practice/cris/functions.js
+++ git-practice/cris/functions.js
@@ -7,7 +7,9 @@
 
   const div = (numberOne,numberTwo) => numberOne / numberTwo;
 
+  const sayHello = (name,firstName,lastName) => print("Hello "+name+" "+firstName+" "+lastName+", Welcome");
+
   const print = string => console.log(string);
 
-  export {print,sum,mul,div};
+  export {print,sum,mul,div,sayHello};
   
