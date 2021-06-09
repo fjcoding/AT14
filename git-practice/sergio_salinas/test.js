1320ff2be2a6fa62853b3a13e98a64ed4fe863af
Sergio <serqio.kay@gmail.com>
2021-06-09 12:46:38 -0400

    Sum of two variables

--- git-practice/sergio_salinas/test.js
+++ git-practice/sergio_salinas/test.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+console.log("Hello World");
+
+var primer_numero;
+var segundo_numero;
+
+
+primer_numero = 1;
+segundo_numero = 2;
+console.log(primer_numero+segundo_numero);
+
