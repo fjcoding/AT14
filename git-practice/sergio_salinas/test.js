69599da959b1ef86584ddf56688a538008dbd1ca
Sergio <serqio.kay@gmail.com>
2021-06-09 13:16:26 -0400

    Transform the int number_to_str to str

--- git-practice/sergio_salinas/test.js
+++ git-practice/sergio_salinas/test.js
@@ -9,5 +9,8 @@ segundo_numero = 2;
 console.log(primer_numero+segundo_numero);
 
 var texto = "qwertyuiopasdfghjklñzxcvvbnm".length;
+console.log("hay "+texto+" letras");
 
-console.log("hay"+texto+"letras");
+var number_to_str = 123456789
+number_to_str.toString
+console.log("asi cambiamos un int a str"+number_to_str);
