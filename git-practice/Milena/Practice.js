db8afa04210cc3473888b168725ca32b834838f4
mlnrdrgz <mile.raychel@gmail.com>
2021-06-09 16:17:51 -0400

    Practice - Second commit

--- git-practice/Milena/Practice.js
+++ git-practice/Milena/Practice.js
@@ -1,4 +1,14 @@
-/* First Exercise*/
+/* First Exercise  basic*/
 function sum(a,b){
-    console.log(a+b);
+    return a+b;
 }
+console.log(sum(1,2));
+
+
+/*function that concatenates two strings*/
+function concatenates(a,b){
+    return `${a} ${b}.`;
+}
+console.log(concatenates('Hello','World'))
+/**/
+/**/
