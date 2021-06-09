69d0747090e546d06b463e6580f4b635826dbb0f
edwinPato <edwin.p.a.pato@gmail.com>
2021-06-09 16:04:16 -0400

    conditional if

--- git-practice/edwin/test.js
+++ git-practice/edwin/test.js
@@ -1,3 +1,6 @@
-let nombreDeLaVariable;
-nombreDeLaVariable = 'Mi nombre es Edwin';
-console.log(nombreDeLaVariable);
+let comida = 'fricase';
+if (comida === 'fricase') {
+  console.log("El fricase es mi favorito");
+} else {
+  console.log("Awwww, pero mi favorito es el fricase...");
+}
