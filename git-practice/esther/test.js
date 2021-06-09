48d1e178a0159fb3ea60715f4c55ee4a2ad78fd6
stellith <hll.esther@gmail.com>
2021-06-09 14:05:09 -0400

    Create a class

--- git-practice/esther/test.js
+++ git-practice/esther/test.js
@@ -1,10 +1,23 @@
-//console.log('hello world');
+class Test{
+    constructor(name, lastName){
+        this.name=name;
+        this.lastName=lastName;
+    }
+    message(){
+        console.log(`Hi my name is ${this.name} and my last name is ${this.lastName}`);
+    }
 
 
-    const numOne=25;
-    const numTwo=5;
-    const sum=  numOne+numTwo;
-    const res= numOne-numTwo;
-    const div= numOne/numTwo;
+    op(){
+        const numOne=25;
+        const numTwo=5;
+        const sum=  numOne+numTwo;
+        const res= numOne-numTwo;
+        const div= numOne/numTwo;
 
-    console.log({sum, res, div});
+        console.log({sum, res, div});
+ }
+}
+let test= new Test("Esther", "Huarayo");
+test.message();
+
