efe83c8fdc0eb901d709e6d79365c80481f3d65b
mlnrdrgz <mile.raychel@gmail.com>
2021-06-09 16:27:24 -0400

    Practice - Third commit

--- git-practice/Milena/Practice.js
+++ git-practice/Milena/Practice.js
@@ -9,6 +9,17 @@ console.log(sum(1,2));
 function concatenates(a,b){
     return `${a} ${b}.`;
 }
-console.log(concatenates('Hello','World'))
+console.log(concatenates('Hello','World'));
+
 /**/
+
+function minTwoNumbers(a,b){
+    if(a < b){
+        return a
+    }
+    else{
+        return b
+    }
+}
+console.log(minTwoNumbers(10,20);
 /**/
