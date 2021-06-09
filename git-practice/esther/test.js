b09d43dbbe34c1fcd8d42c95a8cdf2a75b7ab27a
stellith <hll.esther@gmail.com>
2021-06-09 13:15:57 -0400

    Math operations

--- git-practice/esther/test.js
+++ git-practice/esther/test.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+//console.log('hello world');
+
+
+    const numOne=25;
+    const numTwo=5;
+    const sum=  numOne+numTwo;
+    const res= numOne-numTwo;
+    const div= numOne/numTwo;
+
+    console.log({sum, res, div});
