82606804e3171dd8fd3b907c4054e77270663abc
Sergio <serqio.kay@gmail.com>
2021-06-10 17:42:56 -0400

    Adding arrays in problem

--- args-kata/test.js
+++ args-kata/test.js
@@ -1,8 +1,18 @@
 //Functions 
+var test = "-l -p 8080 -d /usr/logs";
+var test2 = test.split(" ");
+console.log(test2);
 
+for test2.forEach(element =>{console.log(element)   })
+/*
 const functions = (flag,value) => {
 
     switch (value) {
         case "-l":  if(value == true || )
     }
 }
+*/
+
+
+
+
