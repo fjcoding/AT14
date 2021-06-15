8d4630a7909d731b1fb77fd98296de30105e65f8
mlnrdrgz <mile.raychel@gmail.com>
2021-06-15 17:15:25 -0400

    starting code in JavaScript

---
+++ git-practice/Milena/Kata.js
@@ -0,0 +1,16 @@
+class Args{
+    constructor(schema, args){
+        this.args = args;
+        this.schema = schema;
+    }
+
+    parse(){
+        if(this.schema.length === 0 && this.args.length ===0){
+            return;
+        }
+        this.parse
+
+    }
+
+}
+
