e7d5d5c73dd639f3f212347e4edb040e18d5982f
Javier Choque <javier.dreivaj@gmail.com>
2021-06-16 17:00:46 -0400

    Define Value Types for Args Kata

--- args-kata/javier/Type.js
+++ args-kata/javier/Type.js
@@ -1,12 +1,39 @@
 class Type{
-    constructor(args){
-        this.type = args;
+    constructor(str){
+        this.str = str;
+        this.type = "undefined";
+        defineType();
     }
-    isType(){
-        switch(this.type){
-            case "integer": return true;                
-            case "string": return true;
-            default: return false;
+    defineType(){
+        let defaultType = typeof(this.str);
+        switch(defaultType){
+            case "number":  
+                this.type = defaultType; 
+            break;
+            case "boolean":   
+                this.type = defaultType; 
+            break;
+            case "string":    
+                this.type = defaultType; 
+            break;
+            case "object": 
+                if(Array.isArray(this.str)){
+                    let isNumber = true;
+                    value.forEach(element=>{
+                        if(isNaN(element)){
+                            isNumber = false                    
+                        }
+                    });
+                    if(isNumber) this.type = "numberArray";
+                    this.type = "stringArray";
+                }
+            break;
+            default: 
+                this.type = "undefined";
+            break;
         }
+    }
+    getType(){
+        return this.type;
     }
 }
