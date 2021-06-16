cee66df11c2be3fb2124a9aeaa654327ebef6c9a
Carlos Tito Caceres <viri_nxg@hotmail.com>
2021-06-16 17:01:37 -0400

    Separating the functions so one can autocomplete the missings values on schema and the other can detect if the flags fillif the flags can accomplish the schema type of values

--- args-kata/Parse.js
+++ args-kata/Parse.js
@@ -16,6 +16,20 @@ class Parse
             if(flag.id === schema.id)
             {
                 isValid = validate.valueDetector(flag.value) === schema.typeValue;
+  
+            }
+        });
+        return isValid;
+    }
+
+    completeIfEmpty=(schema,flag)=>
+    {
+        const validate = new VarValidate;
+        
+        schema.forEach(schema => 
+        {
+            if(flag.id === schema.id)
+            {
 
                 if(validate.valueCheckEmpty(flag.value))
                 {
@@ -23,17 +37,16 @@ class Parse
                    flag = validate.valueReplaceDefault(flag,defvalue);
                 }
                 //funcion para reemplazar values faltates en el flag
-                isValid = validate.valueDetector(flag.value) === schema.typeValue;
   
             }
         });
-        return isValid;
+        return flag;
     }
 
 }
 
 const defValL = new DefaultValue('-l', false);
-const defValD = new DefaultValue('-d'," ");
+const defValD = new DefaultValue('-d',"asd");
 const defValP = new DefaultValue('-p',0);
 
 const defvalue = [defValL,defValD,defValP];
@@ -42,12 +55,29 @@ const schemaL = new Schema('-l',false,'boolean');
 const schemaP = new Schema('-p',0,'number');
 const schemaD = new Schema('-d','1','string');
 
-const flagL = new Flag('-l');
+/*const flagL = new Flag('-l');
 const flagP = new Flag('-p',2);
-const flagD = new Flag('-d','/usr/logs');
+const flagD = new Flag('-d','/usr/logs');*/
+
+const flagL = new Flag('-l');
+const flagP = new Flag('-p');
+const flagD = new Flag('-d');
 
 const parse = new Parse;
-
+console.log("check if the flag accomplish the schema")
 console.log(parse.isItValid([schemaL,schemaP,schemaD],flagL));
 console.log(parse.isItValid([schemaL,schemaP,schemaD],flagP));
 console.log(parse.isItValid([schemaL,schemaP,schemaD],flagD));
+
+
+console.log("check if the we can autocomplete the missing value")
+
+
+console.log(parse.completeIfEmpty([schemaL,schemaP,schemaD],flagL).id);
+console.log(parse.completeIfEmpty([schemaL,schemaP,schemaD],flagL).value);
+
+console.log(parse.completeIfEmpty([schemaL,schemaP,schemaD],flagP).id);
+console.log(parse.completeIfEmpty([schemaL,schemaP,schemaD],flagP).value);
+
+console.log(parse.completeIfEmpty([schemaL,schemaP,schemaD],flagD).id);
+console.log(parse.completeIfEmpty([schemaL,schemaP,schemaD],flagD).value);
