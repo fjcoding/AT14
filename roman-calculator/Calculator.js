e98abbc067a53174c0f63484200bec967fce09c7
Carlos Tito Caceres <viri_nxg@hotmail.com>
2021-06-22 18:16:22 -0400

    Adding switch on calculator.js

--- roman-calculator/Calculator.js
+++ roman-calculator/Calculator.js
@@ -1,14 +1,18 @@
 class RomanCalculator {
     toDecimalNumber(romanNumeral) {
-        if (romanNumeral === 'V') {
+        /*if (romanNumeral === 'V') {
             return 5
         } else {
             return 1
-        }
+        }*/
         switch(romanNumeral){
             case 'V': return 5;
             case 'I': return 1;
-            
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'D': return 500;
+            case 'C': return 100;
+            case 'M': return 1000;  
         }
     }
 }
