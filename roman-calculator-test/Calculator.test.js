d002139826550064739e9ea0b6d5746d409c10d9
stellith <hll.esther@gmail.com>
2021-06-22 18:05:07 -0400

    Adding test cases

--- roman-calculator-test/Calculator.test.js
+++ roman-calculator-test/Calculator.test.js
@@ -10,3 +10,25 @@ test("Given a roman numeral 'I' Roman Calculator should return 1", () => {
     const calculator = new RomanCalculator
     expect(calculator.toDecimalNumber('I')).toBe(1)
 })
+
+test("Given a roman numeral 'X' Roman Calculator should return 10", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.toDecimalNumber('X')).toBe(10)
+})
+test("Given a roman numeral 'L' Roman Calculator should return 50", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.toDecimalNumber('L')).toBe(50)
+})
+
+test("Given a roman numeral 'D' Roman Calculator should return 500", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.toDecimalNumber('D')).toBe(500)
+})
+test("Given a roman numeral 'C' Roman Calculator should return 100", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.toDecimalNumber('C')).toBe(100)
+})
+test("Given a roman numeral 'M' Roman Calculator should return 1000", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.toDecimalNumber('M')).toBe(1000)
+})
