3afba55f112dbbdcfd67ce7a088e4dcb21192dcc
edwinPato <edwin.p.a.pato@gmail.com>
2021-06-23 12:19:30 -0400

    Function sumRomanNumbers completed

--- roman-calculator-test/Calculator.test.js
+++ roman-calculator-test/Calculator.test.js
@@ -42,3 +42,88 @@ test("Verify that you only enter valid roman character", () => {
     const calculator = new RomanCalculator
     expect(calculator.isCharacterValid('M')).toBe(true)
 })
+
+test("Given a roman number 'CC' romanToDecimal must return 200" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.romanToDecimal('CC')).toBe(200)
+})
+
+test("Given a roman number 'IV' romanToDecimal must return 4" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.romanToDecimal('IV')).toBe(4)
+})
+
+test("Given a roman number 'LXXXIV' romanToDecimal must return 84" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.romanToDecimal('LXXXIV')).toBe(84)
+})
+
+test("Given a roman number 'XCIX' romanToDecimal must return 99" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.romanToDecimal('XCIX')).toBe(99)
+})
+
+test("Given two numbers: 49, 87 sumNumbers must return 136" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.sumNumbers(49, 87)).toBe(136)
+})
+
+test("Given a decimal number 1 decimalToRoman must return 'I'" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.decimalToRoman(1)).toBe('I')
+})
+
+test("Given a decimal number 3 decimalToRoman must return 'III'" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.decimalToRoman(3)).toBe('III')
+})
+
+test("Given a decimal number 4 decimalToRoman must return 'IV'" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.decimalToRoman(4)).toBe('IV')
+})
+
+test("Given a decimal number 5 decimalToRoman must return 'V'" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.decimalToRoman(5)).toBe('V')
+})
+
+test("Given a decimal number 9 decimalToRoman must return 'IX'" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.decimalToRoman(9)).toBe('IX')
+})
+
+test("Given a decimal number 24 decimalToRoman must return 'XXIV'" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.decimalToRoman(24)).toBe('XXIV')
+})
+
+test("Given a decimal number 127 decimalToRoman must return 'CXXVII'" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.decimalToRoman(127)).toBe('CXXVII')
+})
+
+test("Given a decimal number 94 decimalToRoman must return 'CDXXXIV'" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.decimalToRoman(434)).toBe('CDXXXIV')
+})
+
+test("Given a decimal number 94 decimalToRoman must return 'XCIV'" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.decimalToRoman(94)).toBe('XCIV')
+})
+
+test("Given two roman numbers 'X', 'VIII' sumRomanNumbers must return 'XVIII'" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.sumRomanNumbers('X', 'VIII')).toBe('XVIII');
+})
+
+test("Given two roman numbers 'CXXVII', 'XXIV' sumRomanNumbers must return 'CLI'" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.sumRomanNumbers('CXXVII', 'XXIV')).toBe('CLI');
+})
+
+test("Given two roman numbers 'MMDCLXI', 'DLIV' sumRomanNumbers must return 'MMMCCXV'" , () => {
+    const calculator = new RomanCalculator
+    expect(calculator.sumRomanNumbers('MMDCLXI', 'DLIV')).toBe('MMMCCXV');
+})
--- roman-calculator/Calculator.js
+++ roman-calculator/Calculator.js
@@ -18,7 +18,52 @@ class RomanCalculator {
             return true;
         return false;    
     }
-   
+
+    romanToDecimal(romanNumber) {     
+        var decimal = 0;
+        var toDecimalNumber;
+        var previousValue = 0;
+        for (var i = 0; i < romanNumber.length; i++) {
+            toDecimalNumber = this.toDecimalNumber(romanNumber.charAt(i));
+            if(previousValue >= toDecimalNumber)
+                decimal = decimal + toDecimalNumber;                
+            else
+                decimal = decimal + toDecimalNumber - previousValue * 2;
+            previousValue = toDecimalNumber;
+        }
+        return decimal;
+    }
+
+    sumNumbers(number1, number2) {
+        return number1 + number2;
+    }
+
+    sumRomanNumbers(romanNumber1, romanNumber2) {
+        let resultado = this.sumNumbers(this.romanToDecimal(romanNumber1), this.romanToDecimal(romanNumber2));
+        return this.decimalToRoman(resultado);
+    }
+
+    decimalToRoman(number) {
+        let remainder = number;
+        let roman = '';
+        var  romanValues = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']; 
+        var decimalValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+        for(var i = 0; i < romanValues.length; i++) {
+            [roman, remainder] = this.addRomanNumber(remainder, decimalValues[i], romanValues[i], roman);
+        }        
+        return  roman;
+    }
+
+    addRomanNumber(n, decimalnumber, romanEquivalence, romanNumber) {
+        let remainder = n;
+        while(remainder >= decimalnumber)
+        {
+            romanNumber = romanNumber + romanEquivalence;
+            remainder = remainder - decimalnumber;
+        }
+        return [romanNumber, remainder];
+    }   
+    
 }
 
 export { RomanCalculator }
