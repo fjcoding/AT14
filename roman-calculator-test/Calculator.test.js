6842d7d0af751b4b84175ab36b4f44e40c97dcbc
Carlos Tito Caceres <viri_nxg@hotmail.com>
2021-06-23 15:59:10 -0400

    adding the roman number exercise

--- roman-calculator-test/Calculator.test.js
+++ roman-calculator-test/Calculator.test.js
@@ -38,6 +38,102 @@ test("Verify that you only enter valid roman character", () => {
     expect(calculator.isCharacterValid('8')).toBe(false)
 })
 
+test("Verify two numbers can be concatenated to get a bigger one", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.concatenateNumbers('XX','II')).toBe('XXII')
+})
+
+test('Verify that given a roman number it cant has more tan three "I"',()=>{
+    const calculator = new RomanCalculator
+    expect(calculator.lessThanThreeI('III')).toBe(true)
+})
+
+test('Verify that given a roman number it cant has more tan three "X"',()=>{
+    const calculator = new RomanCalculator
+    expect(calculator.lessThanThreeX('XXX')).toBe(true)
+})
+
+test('Verify that given a roman number it cant has more tan three "C"',()=>{
+    const calculator = new RomanCalculator
+    expect(calculator.lessThanThreeC('CCCC')).toBe(false)
+})
+
+test('Verify that given a roman number it can has zero "I"',()=>{
+    const calculator = new RomanCalculator
+    expect(calculator.lessThanThreeI('CCC')).toBe(true)
+})
+
+test('Verify that given a roman number it can has zero "X"',()=>{
+    const calculator = new RomanCalculator
+    expect(calculator.lessThanThreeX('III')).toBe(true)
+})
+
+test('Verify that given a roman number it can has zero "C"',()=>{
+    const calculator = new RomanCalculator
+    expect(calculator.lessThanThreeC('XXX')).toBe(true)
+})
+
+test('Verify that given a roman number it cant has more tan one "V"',()=>{
+    const calculator = new RomanCalculator
+    expect(calculator.lessThanOneV('CCCCV')).toBe(true)
+})
+
+test('Verify that given a roman number it cant has more tan one "L"',()=>{
+    const calculator = new RomanCalculator
+    expect(calculator.lessThanOneL('CCCC')).toBe(true)
+})
+test('Verify that given a roman number it cant has more tan one "D"',()=>{
+    const calculator = new RomanCalculator
+    expect(calculator.lessThanOneD('DD')).toBe(false)
+})
+
+test('Verify that given "IV" roman number it returns 4',()=>{
+
+    const calculator = new RomanCalculator
+    expect(calculator.toSubtractedNumber('IV')).toBe(4)
+})
+
+test('Verify that given "IX" roman number it returns 9',()=>{
+
+    const calculator = new RomanCalculator
+    expect(calculator.toSubtractedNumber('IX')).toBe(9)
+})
+
+test('Verify that given "XL" roman number it returns 40',()=>{
+
+    const calculator = new RomanCalculator
+    expect(calculator.toSubtractedNumber('XL')).toBe(40)
+})
+
+test('Verify that given "XC" roman number it returns 90',()=>{
+
+    const calculator = new RomanCalculator
+    expect(calculator.toSubtractedNumber('XC')).toBe(90)
+})
+
+test('Verify that given "CD" roman number it returns 400',()=>{
+
+    const calculator = new RomanCalculator
+    expect(calculator.toSubtractedNumber('CD')).toBe(400)
+})
+
+test('Verify that given "CM" roman number it returns 900',()=>{
+
+    const calculator = new RomanCalculator
+    expect(calculator.toSubtractedNumber('CM')).toBe(900)
+})
+
+test('Verify that given a roman number it returns a decimal number',()=>{
+
+    const calculator = new RomanCalculator
+    expect(calculator.romanToDecimal('DLVIII')).toBe(558)
+})
+
+test('Verify that given a roman number with lesser number it returns a decimal number',()=>{
+
+    const calculator = new RomanCalculator
+    expect(calculator.romanToDecimal('CCCXCIV')).toBe(394)
+})
 /*
 test("Given two roman numbers I and V Calculator should return 6", () => {
     const calculator = new RomanCalculator;
--- roman-calculator/Calculator.js
+++ roman-calculator/Calculator.js
@@ -1,11 +1,7 @@
 class RomanCalculator {
     
     toDecimalNumber(romanNumeral) {
-        /*if (romanNumeral === 'V') {
-            return 5
-        } else {
-            return 1
-        }*/
+
         switch(romanNumeral){
             case 'V': return 5;
             case 'I': return 1;
@@ -18,11 +14,184 @@ class RomanCalculator {
     }
 
     isCharacterValid(character){
-        let regex = /([I]|[V]|[X]|[L]|[C]|[D]|[M])/;
+        let regex = /([I]|[V]|[X]|[L]|[C]|[D]|[M])/g;
         if(character.search(regex)>=0)
             return true;
         return false;    
     }
+
+    concatenateNumbers(romanNumber1,romanNumber2)
+    {
+        let concatenatedRoman = romanNumber1+romanNumber2;
+        return concatenatedRoman;
+    }
+
+    lessThanThreeI(rumanNumber)
+    {
+        let regex1 = /[I]{1,3}/g;
+        if(rumanNumber.match(regex1)==null)
+        {
+            return true;
+        }
+        else{
+            if(rumanNumber.match(regex1).length > 1 )
+                {return false;}
+            else{return true;}
+        }
+
+    }
+
+    lessThanThreeX(rumanNumber)
+    {
+        let regex1 = /[X]{1,3}/g;
+        if(rumanNumber.match(regex1)==null)
+        {
+            return true;
+        }
+        else{
+            if(rumanNumber.match(regex1).length > 1 )
+                {return false;}
+            else{return true;}
+        }
+
+    }
+
+    lessThanThreeC(rumanNumber)
+    {
+        let regex1 = /[C]{1,3}/g;
+        if(rumanNumber.match(regex1)==null)
+        {
+            return true;
+        }
+        else{
+            if(rumanNumber.match(regex1).length > 1 )
+                {return false;}
+            else{return true;}
+        }
+
+
+    }
+
+    lessThanOneV(rumanNumber)
+    {
+        let regex1 = /[V]{1}/g;
+        if(rumanNumber.match(regex1)==null)
+        {
+            return true;
+        }
+        else{
+            if(rumanNumber.match(regex1).length > 1 )
+                {return false;}
+            else{return true;}
+        }
+
+
+    }
+
+    lessThanOneL(rumanNumber)
+    {
+        let regex1 = /[L]{1}/g;
+        if(rumanNumber.match(regex1)==null)
+        {
+            return true;
+        }
+        else{
+            if(rumanNumber.match(regex1).length > 1 )
+                {return false;}
+            else{return true;}
+        }
+
+
+    }
+
+    lessThanOneD(rumanNumber)
+    {
+        let regex1 = /[D]{1}/g;
+        if(rumanNumber.match(regex1)==null)
+        {
+            return true;
+        }
+        else{
+            if(rumanNumber.match(regex1).length > 1 )
+                {return false;}
+            else{return true;}
+        }
+
+    }
+
+    toSubtractedNumber(romanNumeral) {
+
+        switch(romanNumeral){
+            case 'IV': return 4;
+            case 'IX': return 9;
+            case 'XL': return 40;
+            case 'XC': return 90;
+            case 'CD': return 400;
+            case 'CM': return 900;
+        }
+    }
+//MMLV
+    romanToDecimal(romanNumber)
+    {
+        const roman = new RomanCalculator;
+        let digital=0;
+        let digitalNumber = 0;
+        let value;
+        let lessValue;
+
+        for(let i = 0; i<=romanNumber.length-1; i++)
+        {
+            value = romanNumber.charAt(i);
+            switch(value)
+            {
+                case "I":
+                    if(value == "I" && (romanNumber.charAt(i+1)=="V" || romanNumber.charAt(i+1)=="X"))
+                    {
+                        lessValue = value + romanNumber.charAt(i+1);
+                        digital = roman.toSubtractedNumber(lessValue);
+                        i++;
+                    }
+                    else
+                    {
+                        digital = roman.toDecimalNumber(value)
+                    }
+                    break;
+                case "X":
+                    if(value == "X" && (romanNumber.charAt(i+1)=="L" || romanNumber.charAt(i+1)=="C"))
+                    {
+                        lessValue = value + romanNumber.charAt(i+1);
+                        digital = roman.toSubtractedNumber(lessValue);
+                        i++;
+                    }
+                    else
+                    {
+                        digital = roman.toDecimalNumber(value)
+                    }
+                    break;
+                case "C":
+                    if(value == "C" && (romanNumber.charAt(i+1)=="D" || romanNumber.charAt(i+1)=="M"))
+                    {
+                        lessValue = value + romanNumber.charAt(i+1);
+                        digital = roman.toSubtractedNumber(lessValue);
+                        i++;
+                    }
+                    else
+                    {
+                        digital = roman.toDecimalNumber(value)
+                    }
+                    break;
+                default:
+                    digital = roman.toDecimalNumber(value)
+                    break;
+
+            }
+
+            digitalNumber = digitalNumber + digital;
+        }
+        //console.log(digitalNumber)
+        return digitalNumber;
+    }
+
 }
 
 export { RomanCalculator }
