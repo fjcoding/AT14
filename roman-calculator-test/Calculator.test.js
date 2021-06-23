838bf5f989d4ed43562937f2c34e67811ddb4a83
Sergio <serqio.kay@gmail.com>
2021-06-23 17:14:52 -0400

    Add the next functions toDecimal, veryfyString, getValue,concatenate,limiting with their respective tests

--- roman-calculator-test/Calculator.test.js
+++ roman-calculator-test/Calculator.test.js
@@ -2,89 +2,119 @@ import { RomanCalculator } from "../roman-calculator/Calculator";
 const { test, expect } = require("@jest/globals");
 
 test("Given a roman numeral 'V' Roman Calculator should return 5", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('V')).toBe(5)
-})
+  const calculator = new RomanCalculator();
+  expect(calculator.toDecimal("V")).toBe(5);
+});
 
 test("Given a roman numeral 'I' Roman Calculator should return 1", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('I')).toBe(1)
-})
+  const calculator = new RomanCalculator();
+  expect(calculator.toDecimal("I")).toBe(1);
+});
 
 test("Given a roman numeral 'X' Roman Calculator should return 10", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('X')).toBe(10)
-})
+  const calculator = new RomanCalculator();
+  expect(calculator.toDecimal("X")).toBe(10);
+});
 test("Given a roman numeral 'L' Roman Calculator should return 50", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('L')).toBe(50)
-})
+  const calculator = new RomanCalculator();
+  expect(calculator.toDecimal("L")).toBe(50);
+});
 
 test("Given a roman numeral 'D' Roman Calculator should return 500", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('D')).toBe(500)
-})
+  const calculator = new RomanCalculator();
+  expect(calculator.toDecimal("D")).toBe(500);
+});
 test("Given a roman numeral 'C' Roman Calculator should return 100", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('C')).toBe(100)
-})
+  const calculator = new RomanCalculator();
+  expect(calculator.toDecimal("C")).toBe(100);
+});
 test("Given a roman numeral 'M' Roman Calculator should return 1000", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('M')).toBe(1000)
-})
+  const calculator = new RomanCalculator();
+  expect(calculator.toDecimal("M")).toBe(1000);
+});
 // To numbers with length 2
-test("Given a roman numeral 'CM' Roman Calculator should return 900", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('CM')).toBe(900)
-})
 
-test("Given a roman numeral 'XC' Roman Calculator should return 90", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('XC')).toBe(90)
-})
-test("Given a roman numeral 'IX' Roman Calculator should return 9", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('IX')).toBe(9)
-})
+test("Verify that you enter a invalid roman character, the caracter is 'm'", () => {
+  const calculator = new RomanCalculator();
+  expect(calculator.toDecimal("m")).toBe(undefined);
+});
 
+test("Verify that you enter a invalid roman character, the caracter is '8'", () => {
+  const calculator = new RomanCalculator();
+  expect(calculator.toDecimal("8")).toBe(undefined);
+});
 
-test("Given a roman numeral 'CD' Roman Calculator should return 400", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('CD')).toBe(400)
-})
+test("Make a string whith the next values 'XX' and 'II'", () => {
+  const calculator = new RomanCalculator();
 
-test("Given a roman numeral 'XL' Roman Calculator should return 40", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('XL')).toBe(40)
-})
+  expect(calculator.toPutIntoStrings("XX", "II")).toStrictEqual([
+    ["X", "X"],
+    ["I", "I"]
+  ]);
+});
 
-test("Given a roman numeral 'IV' Roman Calculator should return 4", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('IV')).toBe(4)
-})
+test("Make a string whith the next values 'II' and 'X'", () => {
+  const calculator = new RomanCalculator();
 
-test("Given a roman numeral 'XIV' Roman Calculator should return 14", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('XIV')).toBe(14)
-})
+  expect(calculator.toPutIntoStrings("II", "X")).toStrictEqual([
+    ["I", "I"],
+    ["X"]
+  ]);
+});
 
-test("Given a roman numeral 'LX' Roman Calculator should return 60", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.toDecimalNumber('LX')).toBe(60)
-})
+test("Verify that the characthers giver are Roman numbers -> characters 'XX' y 'II' ", () => {
+  const calculator = new RomanCalculator();
+  const listOfStrings = calculator.toPutIntoStrings("XX", "II");
 
-test("Verify that you only enter valid roman character", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.isCharacterValid('8')).toBe(false)
-})
+  expect(calculator.verifyString(listOfStrings)).toBe(true);
+});
 
+test("Verify that the characthers giver are not Roman numbers -> characters 'XX' y 'I8I' ", () => {
+  const calculator = new RomanCalculator();
+  const listOfStrings = calculator.toPutIntoStrings("XX", "I8I");
 
+  expect(calculator.verifyString(listOfStrings)).toBe(false);
+});
 
-/*
-test("Given two roman numbers I and V Calculator should return 6", () => {
-    const calculator = new RomanCalculator;
-    const number1 = calculator.toDecimalNumber('I');
-    const number2 = calculator.toDecimalNumber('V');
-    expect(calculator.isCharacterValid('8')).toBe(true)
-})
-*/
+test("Verify that the characthers giver are not Roman numbers -> characters 'a' y 'II' ", () => {
+  const calculator = new RomanCalculator();
+  const listOfStrings = calculator.toPutIntoStrings("a", "II");
+
+  expect(calculator.verifyString(listOfStrings)).toBe(false);
+});
+
+test("Get the value of the character -> IX", () => {
+  const calculator = new RomanCalculator();
+
+  expect(calculator.getValue("IX")).toBe(9);
+});
+
+test("Get the value of the character -> CCCLXIV", () => {
+  const calculator = new RomanCalculator();
+
+  expect(calculator.getValue("CCCLXIV")).toBe(364);
+});
+
+test("Concatenate two numbers taking first the bigger number and later de lower one -> 'XX'+'II", () => {
+  const calculator = new RomanCalculator();
+
+  expect(calculator.concatenate("XX", "II")).toBe("XXII");
+});
+
+test("Concatenate two numbers taking first the bigger number and later de lower one -> 'L'+'DC", () => {
+  const calculator = new RomanCalculator();
+
+  expect(calculator.concatenate("L", "DC")).toBe("DCL");
+});
+
+test("Concatenate two numbers taking first the bigger number and later de lower one -> 'I'+'C'", () => {
+  const calculator = new RomanCalculator();
+
+  expect(calculator.concatenate("I", "C")).toBe("CI");
+});
+
+test("put limits on the characters in a row that can appear in a problem ", () => {
+  const calculator = new RomanCalculator();
+
+  expect(calculator.limiting("CCCC")).toBe(false);
+});
--- roman-calculator/Calculator.js
+++ roman-calculator/Calculator.js
@@ -1,52 +1,115 @@
+const listOfRomanNumber = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
+
+const listOfLimits = {
+  I: 3,
+  V: 1,
+  X: 3,
+  L: 1,
+  C: 3,
+  D: 1,
+  M: 9
+};
+
 class RomanCalculator {
-    
-    toDecimalNumber(romanNumeral) {
-        romanNumeral     = Array.from(romanNumeral); 
-        let value        = 0;
-        let lastValue    = this.getNumberValue(romanNumeral[0]);
-        let currentValue = 0;
-        romanNumeral.forEach(element => {
-            currentValue = this.getNumberValue(element);
-            if(lastValue >= currentValue)
-            {
-                value = value + currentValue;
-            }
-            else
-            {
-                value = value - lastValue + (currentValue - lastValue);
-            }    
-            //console.log(currentValue + " ** " + lastValue + " ** " + value);            
-            lastValue = currentValue;
-        });
-        return value;
+  toDecimal = (numeroRomano) => listOfRomanNumber[numeroRomano];
+  toPutIntoStrings = (firstRomanNumber, secondRomanNumber) => {
+    //hacer que sea un numero mayor revisar
+    const togetherNumbers = [];
+    const stringFirstNumber = firstRomanNumber;
+    const splitedFisrt = stringFirstNumber.split("");
+    const stringSecondNumber = secondRomanNumber;
+    const splitedSecond = stringSecondNumber.split("");
+    togetherNumbers.push(splitedFisrt, splitedSecond);
+    return togetherNumbers;
+  };
+  verifyString = (string) => {
+    const listOfValidation = [];
+    const listKeys = Object.keys(listOfRomanNumber);
+    const stringTogether = string;
+    stringTogether.forEach((string) => {
+      const verifiString = string.every((ai) => listKeys.includes(ai));
+      listOfValidation.push(verifiString);
+    });
+    const validate = listOfValidation.includes(false);
+    if (validate === true) {
+      return false;
+    } else {
+      return true;
     }
+  };
 
-    getNumberValue(romanNumeral){
-        switch(romanNumeral){
-            case 'I': return 1;            
-            //case 'IV': return 4;
-            case 'V': return 5;
-            //case 'IX': return 9;
-            case 'X': return 10;
-            //case 'XL': return 40;
-            case 'L': return 50;
-            //case 'XC': return 90;
-            case 'C': return 100;
-            //case 'CD': return 400;
-            case 'D': return 500;
-            //case 'CM': return 900;            
-            case 'M': return 1000;  
-            default: 
-            break;
-        }
-    }
+  getValue = (romanNumber) => {
+    let totalValue = 0;
+    const romanNum = romanNumber;
+    const romanNumSplited = romanNum.split("");
+    romanNumSplited.forEach((num, index, array) => {
+      const firstNumber = this.toDecimal(num);
+      const secondNumber = this.toDecimal(array[index + 1]);
+      if (firstNumber < secondNumber) {
+        totalValue -= firstNumber;
+      } else {
+        totalValue += firstNumber;
+      }
+    });
+    return totalValue;
+  };
 
-    isCharacterValid(character){
-        let regex = /([I]|[V]|[X]|[L]|[C]|[D]|[M])/;
-        if(character.search(regex)>=0)
-            return true;
-        return false;    
+  concatenate = (
+    firstRomanNumberToConcatenate,
+    secondRomanNumbertoConcatenate
+  ) => {
+    const firstNumberTo = firstRomanNumberToConcatenate;
+    const secondNumberTo = secondRomanNumbertoConcatenate;
+    const firstDecimalNum = this.getValue(firstNumberTo);
+    const secondDecimalNum = this.getValue(secondNumberTo);
+    if (firstDecimalNum > secondDecimalNum) {
+      const concatenate = firstNumberTo.concat(secondNumberTo);
+      return concatenate;
+    } else {
+      const concatenate = secondNumberTo.concat(firstNumberTo);
+      return concatenate;
     }
+  };
+
+  limiting = (romanNumber) => {
+    let contadorSeguidos = 0;
+    const romNumber = romanNumber;
+    const romNumberSplit = romNumber.split("");
+    romNumberSplit.forEach((character, index, array) => {
+      const lengthRomNumberSplit = array.length;
+      if (
+        character === array[index + 1] ||
+        array[lengthRomNumberSplit - 2] === array[lengthRomNumberSplit - 3]
+      ) {
+        contadorSeguidos += 1;
+      } else {
+        contadorSeguidos = 0;
+      }
+      console.log(contadorSeguidos);
+      const checking = listOfLimits[character];
+      ///if (arr[lengthRomNumberSplit - 2] === arr[lengthRomNumberSplit - 3])
+
+      if (contadorSeguidos > checking) {
+        return false;
+      } else {
+        contadorSeguidos = 0;
+      }
+    });
+    return true;
+  };
 }
 
-export { RomanCalculator }
+const p = new RomanCalculator();
+const strings = p.toPutIntoStrings("CCC", "II");
+const g = p.limiting("CCCC");
+console.log(g);
+
+export { RomanCalculator };
