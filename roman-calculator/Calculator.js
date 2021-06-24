cfd5db05f9c41a7ddf8ac1fd37c17fe8c2c6f802
Cristian Torrico Vasquez <cristian.torrico@fundacion-jala.org>
2021-06-24 16:06:56 -0400

    Adding some changes on Calculator.js

--- roman-calculator/Calculator.js
+++ roman-calculator/Calculator.js
@@ -3,21 +3,14 @@ class RomanCalculator {
     getNumberValue(romanNumeral = ''){
 
         switch(romanNumeral){
-            case 'I': return 1;            
-            //case 'IV': return 4;
+
+            case 'I': return 1;      
             case 'V': return 5;
-            //case 'IX': return 9;
             case 'X': return 10;
-            //case 'XL': return 40;
             case 'L': return 50;
-            //case 'XC': return 90;
             case 'C': return 100;
-            //case 'CD': return 400;
-            case 'D': return 500;
-            //case 'CM': return 900;            
+            case 'D': return 500;            
             case 'M': return 1000;  
-            default: 
-            break;
         }
     }
 
