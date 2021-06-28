12deb75246b91aebf59e06a6680549aecbe735c0
stellith <hll.esther@gmail.com>
2021-06-28 00:42:01 -0400

    Adding tests to convertDecimalToRoman and sum given two roman numbers

--- roman-calculator-test/Calculator.test.js
+++ roman-calculator-test/Calculator.test.js
@@ -37,15 +37,10 @@ test("Verify that you only enter valid roman character", () => {
     const calculator = new RomanCalculator
     expect(calculator.isCharacterValid('8')).toBe(false)
 })
-
-/*
-test("Given two roman numbers I and V Calculator should return 6", () => {
-    const calculator = new RomanCalculator;
-    const number1 = calculator.toDecimalNumber('I');
-    const number2 = calculator.toDecimalNumber('V');
-    expect(calculator.isCharacterValid('8')).toBe(true)
+test("Verify that you only enter valid roman character", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.isCharacterValid('L')).toBe(true)
 })
-*/
 
 test("Given a roman two numerals 'II' Roman Calculator should return 2", () => {
     const calculator = new RomanCalculator
@@ -86,11 +81,6 @@ test("Given a roman two numeral 'CM' Roman Calculator first one is less than the
     expect(calculator.isLess('CM')).toBe(900)
 })
 
-/*test("Given a roman verify number of digits 'I' can't have more than three", () => {
-    const calculator = new RomanCalculator
-    expect(calculator.verifyNumberDigits('IIII')).toBe(false)
-})*/
-
 test("Given two roman numeral 'I' and 'V' sum roman numbers, should return 6", () => {
     const calculator = new RomanCalculator
     expect(calculator.sumRomanNumeral('I', 'V')).toBe(6)
@@ -110,3 +100,73 @@ test("Given a roman numeral 'LIX' Roman Calculator should return 59", () => {
     const calculator = new RomanCalculator
     expect(calculator.convertRomanToDecimal('LIX')).toBe(59)
 })
+test("Given a roman numeral 'CIX' Roman Calculator should return 109", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.convertRomanToDecimal('CIX')).toBe(109)
+})
+test("Given a roman numeral 'D' Roman Calculator should return 500", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.convertRomanToDecimal('D')).toBe(500)
+})
+test("Given a roman numeral 'M' Roman Calculator should return 1000", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.convertRomanToDecimal('M')).toBe(1000)
+})
+test("Given a roman numeral 'ML' Roman Calculator should return 1050", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.convertRomanToDecimal('ML')).toBe(1050)
+})
+test("Given a roman numeral 'MCX' Roman Calculator should return 1110", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.convertRomanToDecimal('MCX')).toBe(1110)
+})
+test("Given a roman numeral 'MCXV' Roman Calculator should return 1115", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.convertRomanToDecimal('MCXV')).toBe(1115)
+})
+test("Given a roman numeral 'MXXX' Roman Calculator should return 1030", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.convertRomanToDecimal('MXXX')).toBe(1030)
+})
+test("Given a roman numeral 'XIII' Roman Calculator should return 13", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.convertRomanToDecimal('XIII')).toBe(13)
+})
+
+test("Given a decimal number '10' Roman Calculator should return 'X'", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.convertDecimalToRoman(10)).toBe('X')
+})
+
+test("Given a decimal number '50' Roman Calculator should return 'L'", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.convertDecimalToRoman(50)).toBe('L')
+})
+
+test("Given a decimal number '100' Roman Calculator should return 'C'", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.convertDecimalToRoman(100)).toBe('C')
+})
+
+test("Given a decimal number '1500' Roman Calculator should return 'MD'", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.convertDecimalToRoman(1500)).toBe('MD')
+})
+test("Given a decimal number '3516' Roman Calculator should return 'MMMDXVI'", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.convertDecimalToRoman(3516)).toBe('MMMDXVI')
+})
+
+test("Given two roman numeral 'XX' and 'L' sum roman numbers, should return 70", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.sumRomanNumeral('XX', 'L')).toBe(70)
+})
+test("Given two roman numeral 'MXX' and 'LXX' sum roman numbers, should return 1090", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.sumRomanNumeral('MXX', 'LXX')).toBe(1090)
+})
+test("Given two roman numeral 'MCXIX' and 'CLXX' sum roman numbers, should return 1289", () => {
+    const calculator = new RomanCalculator
+    expect(calculator.sumRomanNumeral('MCXIX', 'CLXX')).toBe(1289)
+})
+
