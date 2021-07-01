179555028b6b8417935b0854f6a75d0ceb579484
edwinPato <edwin.p.a.pato@gmail.com>
2021-07-01 17:39:20 -0400

    Refine the description of ball test

--- v2/test/ball.test.js
+++ v2/test/ball.test.js
@@ -1,6 +1,6 @@
 import { Ball } from '../ball.js'
 
-test('Unit test with mocks', () => { 
+test('Give the value 100 as the third argument for the ball constructor, getPaddleY should return 100', () => { 
     const cvs = document.createElement('canvas'); 
     const ctx = cvs.getContext('2d'); 
 
