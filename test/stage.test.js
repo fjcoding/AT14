96acc7f777e42f275a2f42a0bfb0b13eabdfbfbf
Sergio <serqio.kay@gmail.com>
2021-07-05 11:39:05 -0400

    saving advance

--- test/stage.test.js
+++ test/stage.test.js
@@ -1,12 +1,19 @@
-import { drawStage } from "../src/actions/drawStage.js";
+import { Stage } from "../src/models/stage";
 
-test("Verify that Fillrect is called whe set draw is executed", () => {
-  const cvs = { width: 400, height: 200 };
-  const ctx = {
-    fillStyle: undefined,
-    fillRect: jest.fn(),
-    strokeStyle: undefined,
-    strokeRect: jest.fn()
-  };
-  expect(100).toBe(100);
+const cvs = { width: 400, height: 200 };
+const ctx = {
+  fillStyle: undefined,
+  fillRect: jest.fn(),
+  strokeStyle: undefined,
+  strokeRect: jest.fn()
+};
+
+test("Verify that gameOver returns a gameover image", () => {
+  const ball = {};
+  const paddle = {};
+  const stage = new Stage(cvs, ball, paddle, 8);
+
+  const drawingPaddle = new drawPaddle(ctx, cvs, padle);
+  drawingPaddle.setdrawPaddle();
+  expect(ctx.fillRect).toHaveBeenCalled();
 });
