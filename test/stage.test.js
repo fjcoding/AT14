a58116f520e03f20f9e413554128e4d4816efaa4
Sergio <serqio.kay@gmail.com>
2021-07-04 22:26:32 -0400

    I have to change stage tests

--- test/stage.test.js
+++ test/stage.test.js
@@ -1,1 +1,12 @@
 import { drawStage } from "../src/drawStage.js";
+
+test("Verify that Fillrect is called whe set draw is executed", () => {
+  const cvs = { width: 400, height: 200 };
+  const ctx = {
+    fillStyle: undefined,
+    fillRect: jest.fn(),
+    strokeStyle: undefined,
+    strokeRect: jest.fn()
+  };
+  expect(100).toBe(100);
+});
