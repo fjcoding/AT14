f6d3d2177b14403bcaab96179991329d349b9dce
Sergio <serqio.kay@gmail.com>
2021-07-04 23:47:37 -0400

    Adding draw Text function also changed this.bricks value to array to matrix

--- src/drawBricks.js
+++ src/drawBricks.js
@@ -1,30 +1,22 @@
-export class drawBricks
-{
-    
-    constructor(ctx,bricks)
-    {
-        this.ctx    = ctx
-        this.bricks = bricks
-    }
-    
-    setDrawBricks(){
+export class drawBricks {
+  constructor(ctx, bricks) {
+    this.ctx = ctx;
+    this.bricks = bricks;
+  }
 
-        for(let r = 0; r < this.bricks.row; r++){
+  setDrawBricks() {
+    for (let r = 0; r < this.bricks.row; r++) {
+      for (let c = 0; c < this.bricks.column; c++) {
+        let b = this.bricks.bricks[r][c];
 
-            for(let c = 0; c < this.bricks.column; c++){
-
-                let b = this.bricks.bricks[r][c];
-
-                // if the brick isn't broken
-                if(b.status){
-                    
-                    this.ctx.fillStyle = this.bricks.fillColor;
-                    this.ctx.fillRect(b.x, b.y, this.bricks.width, this.bricks.height);
-                    this.ctx.strokeStyle = this.bricks.strokeColor;
-                    this.ctx.strokeRect(b.x, b.y, this.bricks.width, this.bricks.height);
-                }
-            }
+        // if the brick isn't broken
+        if (b.status) {
+          this.ctx.fillStyle = this.bricks.fillColor;
+          this.ctx.fillRect(b.x, b.y, this.bricks.width, this.bricks.height);
+          this.ctx.strokeStyle = this.bricks.strokeColor;
+          this.ctx.strokeRect(b.x, b.y, this.bricks.width, this.bricks.height);
         }
+      }
     }
-
+  }
 }
